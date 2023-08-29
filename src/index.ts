2b6c71f329a73cdc08dc65ec28548da4c4ab2678
luohuidong <luohuidong01@126.com>
2023-08-29 19:45:49 +0800

    refactor: modify folder structure

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import middleware from "./middlewares/index.js";
-import route from "./routes/index.js";
+import registerRouter from "./modules/registerRouter.js";
 
 const app = new Koa();
 middleware(app);
-route(app);
+registerRouter(app);
 
 app.listen(8000, () => {
   console.log("server on 8000");
